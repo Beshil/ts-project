8f6a966cb51fe7d5ec679d49ad62f48376846d72
Beshil <ersanbievbesh@gmail.com>
2023-02-27 23:25:45 +0300

    add Loader & page notFound

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,12 +1,13 @@
-import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared/congif/routeConfig/routeConfig';
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/congif/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader";
 
 const AppRouter = () => {
   const { t } = useTranslation();
   return (
-    <Suspense fallback={<div>{t('Loading...')}</div>}>
+    <Suspense fallback={<PageLoader />}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
           <Route
