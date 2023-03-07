a67a5141c4a1acce567daa74cff40221a7a47c2a
Beshil <ersanbievbesh@gmail.com>
2023-03-07 16:39:23 +0300

    add storybook

---
+++ src/shared/ui/AppLink/Applink.stories.tsx
@@ -0,0 +1,57 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Theme } from "app/providers/ThemeProvider";
+import { ThemeDecorator } from "shared/congif/storybook/ThemeDecorator/ThemeDecorator";
+
+import { AppLink, AppLinkTheme } from "./AppLink";
+
+export default {
+  title: "shared/AppLink",
+  component: AppLink,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+  args: {
+    to: "/",
+  },
+} as ComponentMeta<typeof AppLink>;
+
+const Template: ComponentStory<typeof AppLink> = (args) => (
+  <AppLink {...args} />
+);
+
+export const PRIMARY = Template.bind({});
+PRIMARY.args = {
+  children: "text",
+  theme: AppLinkTheme.PRIMARY,
+};
+
+export const RED = Template.bind({});
+RED.args = {
+  children: "text",
+  theme: AppLinkTheme.RED,
+};
+
+export const SECONDARY = Template.bind({});
+SECONDARY.args = {
+  children: "text",
+  theme: AppLinkTheme.SECONDARY,
+};
+
+export const PRIMARYDARK = Template.bind({});
+PRIMARYDARK.args = {
+  children: "text",
+  theme: AppLinkTheme.PRIMARY,
+};
+PRIMARYDARK.decorators = [ThemeDecorator(Theme.DARK)];
+export const REDDARK = Template.bind({});
+REDDARK.args = {
+  children: "text",
+  theme: AppLinkTheme.RED,
+};
+REDDARK.decorators = [ThemeDecorator(Theme.DARK)];
+export const SECONDARYDARK = Template.bind({});
+SECONDARYDARK.args = {
+  children: "text",
+  theme: AppLinkTheme.SECONDARY,
+};
+SECONDARYDARK.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ src/widgets/PageError/ui/PageError.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Theme } from "app/providers/ThemeProvider";
+import { ThemeDecorator } from "shared/congif/storybook/ThemeDecorator/ThemeDecorator";
+
+import { PageError } from "./PageError";
+
+export default {
+  title: "widget/PageError",
+  component: PageError,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof PageError>;
+
+const Template: ComponentStory<typeof PageError> = (args) => (
+  <PageError {...args} />
+);
+
+export const Lite = Template.bind({});
+Lite.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
